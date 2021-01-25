e14f84d285633b9eceb1bef738e2f3ea1eebea48
David Bassey <davidosky1@gmail.com>
2021-01-25 15:18:39 +0100

    creates event_attendances controller

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,2 +1,15 @@
 class EventAttendancesController < ApplicationController
+  def new
+    @event_attendance = EventAttendance.new
+  end
+
+  def create
+    @event_attendance = EventAttendance.create(event_attendance_params)
+  end
+
+  private
+
+  def event_attendance_params
+    params.require(:event_attendance).permit(:attendee_id, :event_attended_id)
+  end
 end
